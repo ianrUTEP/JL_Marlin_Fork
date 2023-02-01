063f25c76df5d07d2bb707e21736a9290d7165fd
Ian Rybak <iarybak@miners.utep.edu>
2023-01-31 18:53:20 -0700

    More changes as MP2.1 gets deployed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -39,6 +39,7 @@
 #define BTT_MOTOR_EXPANSION            // Added by config.ini
 #define NO_AUTO_ASSIGN_WARNING         // Added by config.ini
 #define NO_CONFIGURATION_EMBEDDING_WARNING // Added by config.ini
+#define MARLIN_EEPROM_SIZE 0x2000      // Added by config.ini
 
 //===========================================================================
 //============================= Getting Started =============================
@@ -123,7 +124,7 @@
  * Currently Ethernet (-2) is only supported on Teensy 4.1 boards.
  * :[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-//#define SERIAL_PORT_2 off ; Unused
+#define SERIAL_PORT_2 -1
 //#define BAUDRATE_2 250000   // :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000] Enable to override BAUDRATE
 
 /**
