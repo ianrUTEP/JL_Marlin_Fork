f5f02a2f81e75a17085a5ec6f059fd1685dec8df
Ian Rybak <iarybak@miners.utep.edu>
2024-05-23 14:20:59 -0600

    Save Z position in the code
    Behavior does not appear correct, but will investigate to see if it is expected.

--- Marlin/src/gcode/motion/G101.cpp
+++ Marlin/src/gcode/motion/G101.cpp
@@ -47,7 +47,8 @@ extern xyze_pos_t destination;
  */
 void GcodeSuite::G101(TERN_(HAS_FAST_MOVES, const bool fast_move/*=false*/)) {
   static const char z_axis_codes[] = { LIST_N(NUM_Z_STEPPERS, 'U', 'V', 'W') };
-  static float change = 0; //A variable to store changes of each z stepper
+  static float change = 0; // A variable to store changes of each z stepper
+  static float last_z = 0; // A variable to track the last known z before performing the adjustments
 
   if (!MOTION_CONDITIONS) return;
 
@@ -73,6 +74,7 @@ void GcodeSuite::G101(TERN_(HAS_FAST_MOVES, const bool fast_move/*=false*/)) {
   for (int i = 0; i < NUM_Z_STEPPERS; i++) {
     // Check for pressence of current stepper
     if (parser.seenval(z_axis_codes[i])) {
+      last_z = current_position.z;
       // Change is the difference between the provided value and the stored value
       // In this way, only the required motion for each axis will be done
       // The provided value is then stored to the array holding the difference positions
@@ -84,7 +86,8 @@ void GcodeSuite::G101(TERN_(HAS_FAST_MOVES, const bool fast_move/*=false*/)) {
       change = z_deltas[i] - parser.floatval(z_axis_codes[i]);
       do_blocking_move_to_z(change + current_position.z);
       stepper.set_all_z_lock(false);  // Unlock all axes
-      z_deltas[i] = parser.floatval(z_axis_codes[i]); // Save the cumulative position
+      z_deltas[i] = parser.floatval(z_axis_codes[i]); // Save the new delta position
+      current_position.z = last_z;
     }
   }
   stepper.set_separate_multi_axis(false); // Relock all the steppers
