5c62d5a855a2235bdd2a7548088004e0a94aab92
Ian Rybak <iarybak@miners.utep.edu>
2024-05-23 18:30:53 -0600

    Tilt feedrate and comments
    Tilt feedrate can now be specified with a Q value in the command, with or without other parameters in G101.
    Tilt feedrate only affects moves that are the actual tilt, everything else is handled as a normal G1 command.
    
    In the future, the best solution would be to abstract this speed control to motion.cpp, where the other feedrates are handled, as well as provide a parameter for default feedrate to be configured in the configuration.h file.

--- Marlin/src/gcode/motion/G101.cpp
+++ Marlin/src/gcode/motion/G101.cpp
@@ -49,6 +49,7 @@ void GcodeSuite::G101(TERN_(HAS_FAST_MOVES, const bool fast_move/*=false*/)) {
   static const char z_axis_codes[] = { LIST_N(NUM_Z_STEPPERS, 'U', 'V', 'W') };
   static float change = 0; // A variable to store changes of each z stepper
   static float last_z = 0; // A variable to track the last known z before performing the adjustments
+  static feedRate_t tilt_fr_mm_s = 9;  // Feedrate of the tilting. Stored and evaluated in mm/s
 
   if (!MOTION_CONDITIONS) return;
 
@@ -64,10 +65,19 @@ void GcodeSuite::G101(TERN_(HAS_FAST_MOVES, const bool fast_move/*=false*/)) {
     #endif
   #endif
 
+  // Include the O flag to first reset the current displacements to 0.
+  // Primarily for testing purposes only. 
   if (parser.seen('O')) {
     for(int i = 0; i < NUM_Z_STEPPERS; i++) {
       z_deltas[i] = 0;
     }
+  }
+
+  // Include a Q value to set the feedrate of the tilting moves. 
+  // If not included, the last known will be used
+  // The Q value should be provided in GCODE as mm/min, the value_feedrate() automatically converts to mm/s
+  if (parser.floatval('Q') > 0) {
+    tilt_fr_mm_s = parser.value_feedrate();
   }
 
   stepper.set_separate_multi_axis(true);  //Separate Z axes
@@ -84,9 +94,11 @@ void GcodeSuite::G101(TERN_(HAS_FAST_MOVES, const bool fast_move/*=false*/)) {
       // plus the necessary change to get to the delta location
       stepper.set_all_z_lock(true, i);  // Lock all except current Z stepper
       change = z_deltas[i] - parser.floatval(z_axis_codes[i]);
-      do_blocking_move_to_z(current_position.z - change);
+      do_blocking_move_to_z(current_position.z - change, tilt_fr_mm_s);
       stepper.set_all_z_lock(false);  // Unlock all axes
       z_deltas[i] = parser.floatval(z_axis_codes[i]); // Save the new delta position
+      // Reset the current position to the last known
+      // Also must set the current position in the planner or else it will execute a move
       current_position.z = last_z;
       planner.set_position_mm(current_position);
     }
